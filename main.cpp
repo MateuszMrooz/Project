4c07e5fa508acbbd7a743adafe87c0ce126bf04b
MateuszMrooz <20mmroz.acc@lwetb.ie>
2026-03-12 17:33:36 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -16,10 +16,11 @@ void setup() {
   Serial.begin(9600); // Initialize serial communication
   DHT11_Sensor.begin();
   delay(2000);// Wait 2 seconds after starting
-  pinMode(7, OUTPUT);// Sets the digital pins as output for LEDs
-  pinMode(6, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(4, OUTPUT);
+  //Sets the digital pins as output for LEDs
+  pinMode(7, OUTPUT);//Low risk LED
+  pinMode(6, OUTPUT);//Moderate risk LED
+  pinMode(5, OUTPUT);//High risk LED
+  pinMode(4, OUTPUT);//Fire LED
   Start_time = millis();  //Initial start time
   }
 
@@ -30,112 +31,117 @@ Temp = DHT11_Sensor.readTemperature(false);//Read DHT temperature and humidity, 
 Humidity = DHT11_Sensor.readHumidity();
 
 Current_time = millis();
-if (Current_time - Start_time > 2000) //Small time delay to avoid breaking DHT11 sensor
+if (Current_time - Start_time > 2000) //Small time delay to avoid breaking DHT11 sensor without delay
 
 {
-Serial.print(" Temperature: ");
-Serial.print(Temp);
-Serial.print(" Humidity: ");
-Serial.print(Humidity);
-Serial.print(" Light_intensity: ");
-Serial.print(Light);
-Serial.print(" Wind: ");
-Serial.println(Wind/20); //Dividing by 20 gives the windspeed a more realisic value
-Start_time = millis(); 
+  Serial.print(" Temperature: ");
+  Serial.print(Temp);
+  Serial.print(" Humidity: ");
+  Serial.print(Humidity);
+  Serial.print(" Light_intensity: ");
+  Serial.print(Light);
+  Serial.print(" Wind: ");
+  Serial.println(Wind/20); //Dividing by 20 gives the windspeed a more realisic value
+  Start_time = millis(); 
 }
 
-  if (Serial.available() > 0) {
-    // read the incoming byte as LED_data:
-    LED_data = Serial.read();
+if (Serial.available() > 0) {
+LED_data = Serial.read();//Read the incoming byte as LED_data:
+
+
+}
+    
+
+    switch (LED_data)
+    {
+
+    
+    case 49://49 is ASCII for 1 which corresponds to the serial write code in Thonny in the Risk file (Risk_level and Fire functions)
+      digitalWrite(7,LOW);
+      digitalWrite(6,LOW);
+      digitalWrite(5,LOW);
+      digitalWrite(4,LOW);
+      digitalWrite(7,HIGH);
+      digitalWrite(7,HIGH);
+      delay(500);
+      digitalWrite(7,LOW);
+      delay(500);
+      digitalWrite(7,HIGH);//Keeps LED on constantly until next signal
+      break;
+      
+    
+    case 50:
+      digitalWrite(7,LOW);
+      digitalWrite(6,LOW);
+      digitalWrite(5,LOW);
+      digitalWrite(4,LOW);
+      digitalWrite(7,HIGH);
+      break;
+   
+
+    case 51://All of these if statements do the same for all of the other LEDs with a odd number(both in python and here) being a constant on and an even being a flashing LED
+      digitalWrite(7,LOW);
+      digitalWrite(6,LOW);
+      digitalWrite(5,LOW);
+      digitalWrite(4,LOW);
+      digitalWrite(7,HIGH);
+      digitalWrite(6,HIGH);
+      delay(500);
+      digitalWrite(6,LOW);
+      delay(500);
+      digitalWrite(6,HIGH);
+      break;
+    
+
+    case 52:
+      digitalWrite(7,LOW);
+      digitalWrite(6,LOW);
+      digitalWrite(5,LOW);
+      digitalWrite(4,LOW);
+      digitalWrite(7,HIGH);
+      digitalWrite(6,HIGH);
+      break;
+    
+      
+    case 53:
+      digitalWrite(7,LOW);
+      digitalWrite(6,LOW);
+      digitalWrite(5,LOW);
+      digitalWrite(4,LOW);
+      digitalWrite(5,HIGH);
+      digitalWrite(6,HIGH);
+      digitalWrite(7,HIGH);
+      delay(500);
+      digitalWrite(5,LOW);
+      delay(500);
+      digitalWrite(5,HIGH);
+      break;
+    
+
+    case 54:
+      digitalWrite(7,LOW);
+      digitalWrite(6,LOW);
+      digitalWrite(5,LOW);
+      digitalWrite(4,LOW);
+      digitalWrite(5,HIGH);
+      digitalWrite(6,HIGH);
+      digitalWrite(7,HIGH);
+      break;
+
+
+    case 55:
+      digitalWrite(4,LOW);
+      digitalWrite(4,HIGH);
+      break;
+
+
+    case 56:
+      digitalWrite(4,LOW);//Turns off all LEDs when there is no risk
+      digitalWrite(5,LOW);
+      digitalWrite(6,LOW);
+      digitalWrite(7,LOW);
+      break;
+    }
+  
+
   }
-    if (LED_data == 49){//49 is ASCII for 1 which corresponds to the serial write code in Thonny in the Risk file (Risk_level function)
-      digitalWrite(7,LOW);
-      digitalWrite(6,LOW);
-      digitalWrite(5,LOW);//Resets all of the LEDs
-      digitalWrite(4,LOW);
-      digitalWrite(7,HIGH);//LED on, 7 represtents digital pin 7 which is what one of my LEDs in connected to
-      delay(500);//Delay to make LED blink
-      digitalWrite(7,LOW);//LED off to complete blink
-      delay(500);
-      digitalWrite(7,HIGH);//Stays on and repeats due to being in a loop until new data is sent from lines 39-47(every 2 seconds)
-    }
-  
-    if (LED_data == 50){
-      digitalWrite(7,LOW);
-      digitalWrite(6,LOW);
-      digitalWrite(5,LOW);
-      digitalWrite(4,LOW);
-      digitalWrite(7,HIGH);//Keeps LED on constantly
-    }
-
-    if (LED_data == 51){//All of these if statements do the same for all of the other LEDs with a odd number being a constant on and an even being a flashing LED
-      digitalWrite(7,LOW);
-      digitalWrite(6,LOW);
-      digitalWrite(5,LOW);
-      digitalWrite(4,LOW);
-      digitalWrite(7,HIGH);
-      digitalWrite(6,HIGH);
-      delay(500);
-      digitalWrite(6,LOW);
-      delay(500);
-      digitalWrite(6,HIGH);
-    }
-
-    if (LED_data == 52){
-      digitalWrite(7,LOW);
-      digitalWrite(6,LOW);
-      digitalWrite(5,LOW);
-      digitalWrite(4,LOW);
-      digitalWrite(7,HIGH);
-      digitalWrite(6,HIGH);
-    }
-    
-    if (LED_data == 53){
-      digitalWrite(7,LOW);
-      digitalWrite(6,LOW);
-      digitalWrite(5,LOW);
-      digitalWrite(4,LOW);
-      digitalWrite(5,HIGH);
-      digitalWrite(6,HIGH);
-      digitalWrite(7,HIGH);
-      delay(500);
-      digitalWrite(5,LOW);
-      delay(500);
-      digitalWrite(5,HIGH);
-    }
-    
-    if (LED_data == 54){
-      digitalWrite(7,LOW);
-      digitalWrite(6,LOW);
-      digitalWrite(5,LOW);
-      digitalWrite(4,LOW);
-      digitalWrite(5,HIGH);
-      digitalWrite(6,HIGH);
-      digitalWrite(7,HIGH);
-    }
-
-    if (LED_data == 55){
-      digitalWrite(7,LOW);
-      digitalWrite(6,LOW);
-      digitalWrite(5,LOW);
-      digitalWrite(4,LOW);
-      digitalWrite(4,HIGH);
-      delay(500);
-      digitalWrite(4,LOW);
-      delay(500);
-      digitalWrite(4,HIGH);
-    }
-
-    if (LED_data == 56){
-      digitalWrite(7,LOW);
-      digitalWrite(6,LOW);
-      digitalWrite(5,LOW);
-      digitalWrite(4,LOW);
-      digitalWrite(4,HIGH);
-
-    }
-    
-    
-
-  
-}
